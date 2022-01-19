3eb44509392b5c108a48923f1e6e983925bb5b35
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-01-19 20:38:31 +0800

    Completed requirement in range task

--- L1_control_flow/range.rb
+++ L1_control_flow/range.rb
@@ -1,4 +1,12 @@
 print "Select a number from 0 - 100 : "
 number = gets.chomp.to_i
 
-if number < 0
+if number >= 0 && number <= 50
+    puts "#{number} is between 0 and 50"
+elsif number >= 51 && number <= 100 
+    puts "#{number} is between 51 and 100"
+elsif number > 100
+    puts "#{number} is greater than 100"
+else
+    puts "#{number} is less than 0"
+end
